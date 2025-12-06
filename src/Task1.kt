f9e23b517959b6fcaa46261f0e4d09ab13998665
kirill <holomeeev2011@gmail.com>
2025-12-06 12:13:34 +0300

    The Beginning.

---
+++ src/Task1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val d1 = Point(1,1)
+    val d2 = Point(-4,2)
+    val d3 = Point(0,4)
+
+    val tri = Triangle(d1, d2, d3)
+
+    val dp = Point(0, 3)
+
+
+}
---
+++ src/Triangle.kt
@@ -0,0 +1,5 @@
+class Triangle(dot1: Point, dot2: Point, dot3: Point) {
+    var dot1 = dot1
+    var dot2 = dot2
+    var dot3 = dot3
+}
